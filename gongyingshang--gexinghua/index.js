205b12bd5f12656d8a903fdddd77f7d8537be471
zhaobao1830 <x5219438>
2017-04-10 23:35:07 +0800

    供应商个性化页面

---
+++ gongyingshang--gexinghua/index.js
@@ -0,0 +1,73 @@
+$(function () {
+    $(".busNedds li").on("click",function () {
+        if($(this).hasClass("liChecked")){
+            $(this).removeClass("liChecked");
+            $(this).find('i').removeClass("iChecked");
+            $(this).find('p').removeClass("pChecked");
+        }else{
+            $(this).addClass("liChecked");
+            $(this).find('i').addClass("iChecked");
+            $(this).find('p').addClass("pChecked");
+        }
+    })
+})
+
+function formSubmit() {
+    var nameVal = $(".name-input").val(),  //公司全称
+        contactsVal = $(".contacts").val(),  //联系人
+        telephoneVal = $(".telephone").val(), //手机号
+        emailVal = $(".email").val(),
+        addressVal = $(".address").val(), //所在地区
+        bussNedds = [], //业务需求
+        busDescription = $(".busDescription").val() //业务描述
+       //获取选择的业务需求
+        $(".busNedds li").each(function () {
+            if($(this).hasClass("liChecked")){
+                bussNedds.push($(this).find("p").text().trim())
+            }
+        })
+    var flag = true;
+    //判断公司全称是否为空
+    if(nameVal){
+        flag = true;
+        $(".name-pro").removeClass("proShow").addClass("proHide");
+    }else{
+        flag = false;
+        $(".name-pro").removeClass("proHide").addClass("proShow");
+    }
+    //判断联系人是否为空
+    if(contactsVal){
+        flag = true;
+        $(".contacts-pro").removeClass("proShow").addClass("proHide");
+    }else{
+        flag = false;
+        $(".contacts-pro").removeClass("proHide").addClass("proShow");
+    }
+    //判断手机号是否为空
+    if(telephoneVal){
+        flag = true;
+        $(".tel-pro").removeClass("proShow").addClass("proHide");
+    }else{
+        flag = false;
+        $(".tel-pro").removeClass("proHide").addClass("proShow");
+    }
+    //判断邮箱是否为空
+    if(emailVal){
+        flag = true;
+        $(".email-pro").removeClass("proShow").addClass("proHide");
+    }else{
+        flag = false;
+        $(".email-pro").removeClass("proHide").addClass("proShow");
+    }
+    if(flag) {
+        $.ajax({
+            url: "",
+            type: "post",
+            data: {},
+            success: function (data) {
+
+            }
+
+        })
+    }
+}
