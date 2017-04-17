7fd7218fc878e2ef2e8958a7774a003eabda3ae9
赵宝 <zhaobao@lubanec.com>
2017-04-17 16:06:44 +0800

    左侧页面完成

--- houtaiguanli/left/left.js
+++ houtaiguanli/left/left.js
@@ -2,11 +2,16 @@ $(function () {
     var clickId //点击的元素的id
     var clickClass //点击的元素的class
     $(".management").on("click",function () {
+        console.log("tt")
         clickId=$(this).attr("id")
         clickClass=$("."+clickId)
         if(clickClass.hasClass("displayNone")){
+            $(this).find("i").removeClass("iNoClick").addClass("iClick")
+            $(this).find("p").removeClass("pNoClick").addClass("pClick")
             clickClass.removeClass("displayNone").addClass("displayBlock")
         }else{
+            $(this).find("i").removeClass("iClick").addClass("iNoClick")
+            $(this).find("p").removeClass("pClick").addClass("pNoClick")
             clickClass.removeClass("displayBlock").addClass("displayNone")
         }
     })
