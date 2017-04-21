eea6212aa8b54361e87d99fd54676b3f88ce3e2d
赵宝 <zhaobao@lubanec.com>
2017-04-21 10:17:57 +0800

    服务注册管理

---
+++ houtaiguanli/serviceRegistry/serviceRegistry.js
@@ -0,0 +1,44 @@
+$(function () {
+    var sregContent_height=$(".left").height()
+    // 获取sregContent高度
+    $(".sregContent").height(sregContent_height)
+    // 获取sregCon高度
+    var sregCon_height=sregContent_height-158
+    $(".sregCon").height(sregCon_height)
+
+
+
+    //给thead tr img绑定click事件
+    $("thead tr th img").on("click",function () {
+        if($(this).hasClass("thiNoClick")){
+            $(this).removeClass("thiNoClick")
+            $(this).attr("src","img/sre_032.png")
+            $("tbody tr td img").each(function () {
+                $(this).removeClass("triNoClick")
+                $(this).attr("src","img/sre_042.png")
+                $(this).parent().parent().addClass("trClick")
+            })
+        }else{
+            $(this).removeClass("thiClick")
+            $(this).attr("src","img/sre_031.png")
+            $("tbody tr td img").each(function () {
+                $(this).removeClass("triClick")
+                $(this).attr("src","img/sre_041.png")
+                $(this).parent().parent().removeClass("trClick")
+            })
+        }
+    })
+    // 给tbody下的img绑定click事件，点击的时候判断，有triNoClick就添加triClick，tr背景改变
+    $("tbody tr td img").on("click",function () {
+        if($(this).hasClass("triNoClick")){
+            $(this).removeClass("triNoClick")
+            $(this).attr("src","img/sre_042.png")
+            $(this).parent().parent().addClass("trClick")
+        }else{
+            $(this).removeClass("triClick")
+            $(this).attr("src","img/sre_041.png")
+            $(this).parent().parent().removeClass("trClick")
+        }
+    })
+
+})
