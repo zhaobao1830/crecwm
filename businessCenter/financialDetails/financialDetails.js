521bc343b2d4339e8ae4019630a59cc2afe6b727
赵宝 <zhaobao@lubanec.com>
2017-06-02 15:13:35 +0800

    数据中心完成

--- businessCenter/financialDetails/financialDetails.js
+++ businessCenter/financialDetails/financialDetails.js
@@ -1,8 +1,17 @@
 $(function () {
+  //日期选择事件
   $('.fdcSearch > div > div > a').on('click',function () {
     if (!$(this).hasClass('aClick')) {
       $(this).siblings().removeClass('aClick')
       $(this).addClass('aClick')
+    }
+  })
+
+  // 收入 退款点击事件
+  $(".fdcResult-top1 > a").on('click',function () {
+    if(!$(this).hasClass('top_aClick')){
+      $(this).siblings().removeClass('top_aClick')
+      $(this).addClass('top_aClick')
     }
   })
 })
--- businessCenter/financialSummary/financialSummary.js
+++ businessCenter/financialSummary/financialSummary.js
@@ -1,8 +1,19 @@
 $(function () {
   $(".fscSearch > div >a").on("click",function () {
+    var clickId=''
     if($(this).hasClass("searchANoclick")){
       $(this).siblings().removeClass("searchAclick").addClass("searchANoclick")
       $(this).removeClass("searchANoclick").addClass("searchAclick")
+      clickId=$(this).prop('id')
+      $(".createCode_date_start").val('')
+      $("."+clickId+'_date').removeClass('displayNone').addClass('displayBlock')
+      $("."+clickId+'_date').siblings().removeClass('displayBlock').addClass('displayNone')
     }
   })
 })
+//悬着日期后触发的事件
+function dayChange(val) {
+  if(val){
+    //执行方法
+  }
+}
