99f425df09744bf109c002b965ae0c01298a8045
赵宝 <zhaobao@lubanec.com>
2017-06-08 17:47:36 +0800

    修改财务汇总

--- businessCenter/dataCenter/js/dataCenter.js
+++ businessCenter/dataCenter/js/dataCenter.js
@@ -15,13 +15,23 @@ function getData() {
    * Highcharts 在 4.2.0 开始已经不依赖 jQuery 了，直接用其构造函数既可创建图表
    3
    **/
+  $.ajax({
+    url:'',
+    type:'post',
+    data:{}, //参数
+    // dataType:"json",  如果是json格式的，就加这个
+    success:function (data) {
+      var categories=[]
+      categories.push(data.ss)
+    }
+  })
   var chart = new Highcharts.Chart('dcResult-show', {
     title: {
     text: '销售总金额（元）'
   },
-  colors: ['#cb2a2d'],
+  colors: ['#cb2a2d','#000'],
   xAxis: {
-    categories: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月']
+    categories: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月','13']
   },
   yAxis: {
     title: {
@@ -42,9 +52,15 @@ function getData() {
     verticalAlign: 'middle',
     borderWidth: 0
   },
-  series: [{
+  series: [
+    {
     name: '东京',
-    data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-  }]
+    data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6, 10],
+    },
+    {
+      name: '柏林',
+      data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0,11]
+    }
+  ]
 });
 }
--- businessCenter/financialSummary/financialSummary.js
+++ businessCenter/financialSummary/financialSummary.js
@@ -1,4 +1,7 @@
 $(function () {
+  //页面刚打开时调用查询方法
+  searchResult()
+
   $(".fscSearch > div >a").on("click",function () {
     var clickId=''
     if($(this).hasClass("searchANoclick")){
@@ -8,12 +11,44 @@ $(function () {
       $(".createCode_date_start").val('')
       $("."+clickId+'_date').removeClass('displayNone').addClass('displayBlock')
       $("."+clickId+'_date').siblings().removeClass('displayBlock').addClass('displayNone')
+      $(".datePickerImg").removeClass("displayNone").addClass("displayBlock")
     }
+    //点击日汇总、月汇总后调用查询方法
+    searchResult()
   })
 })
-//悬着日期后触发的事件
+//选择日期后触发的事件
 function dayChange(val) {
   if(val){
     //执行方法
+    searchResult()
   }
+}
+//请求后台数据
+function searchResult() {
+  var classFica='' //判断现在选择的是日汇总还是月汇总
+  classFica=$(".searchAclick").text().trim()
+  var date='' //通过不同的classFica选择不同的时间
+  var checkDate='' //当前选择的时间
+  checkDate=$("#"+$(".searchAclick").attr('id')+'_date').val()
+  var myDate = new Date()
+  if(checkDate){
+    date=checkDate
+  }else{
+    if($(".searchAclick").attr('id') === 'daily'){
+      date=myDate.getFullYear()+'-'+(myDate.getMonth()+1)
+    }else{
+      date=myDate.getFullYear()
+    }
+  }
+  $.ajax({
+    url:'',
+    data:{classFica:classFica,date:date},
+    type:'post',
+    contentType:"application/json",
+    dataType:"json",
+    success:function (data) {
+
+    }
+  })
 }
