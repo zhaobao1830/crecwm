a34f711ec7f1b967220da7bf024bd9a9c8e8c25a
赵宝 <zhaobao@lubanec.com>
2018-05-02 09:13:51 +0800

    网校

--- onlineSchool/index/index.js
+++ onlineSchool/index/index.js
@@ -13,11 +13,17 @@ $(function () {
   })
 
   // 咨询列表 头部
-  $(".list_title").on('mouseover', function () {
-    $(".list_title > span > img").attr('src','img/jiantouYes.png')
+  $(".informationList_left_title").on('mouseover', function () {
+    $(".informationList_left_title > a > img").attr('src','img/jiantouYes.png')
   })
-  $(".list_title").on('mouseout', function () {
-    $(".list_title > span > img").attr('src','img/jiantouNo.png')
+  $(".informationList_left_title").on('mouseout', function () {
+    $(".informationList_left_title > a > img").attr('src','img/jiantouNo.png')
+  })
+  $(".informationList_right_title").on('mouseover', function () {
+    $(".informationList_right_title > a > img").attr('src','img/jiantouYes.png')
+  })
+  $(".informationList_right_title").on('mouseout', function () {
+    $(".informationList_right_title > a > img").attr('src','img/jiantouNo.png')
   })
 })
 
@@ -25,25 +31,21 @@ $(function () {
 function onlineDisplayChange(className) {
   // 使用了jquery.rotate.min插件
   $("."+className).on('mouseover',function () {
+    $(".img_back_"+className).css({"backgroundImage":"url(./img/frame_1_0.png)"})
+    $(".img_show_"+className).find('img').attr('src','img/'+className+'_1_1.png')
+    $(".key_value_"+className).find('p').css({'color':'#398fd1'})
     $(".img_back_"+className).rotate({
-      animateTo: -90,
-      callback: function () {
-        $(this).css({"backgroundImage":"url(./img/frame_1_0.png)","transform": "rotate(0)"})
-        $(".img_show_"+className).find('img').attr('src','img/'+className+'_1_1.png')
-        $(".key_value_"+className).find('p').css({'color':'#398fd1'})
-      }
+      animateTo: -90
     })
   })
   $("."+className).on('mouseout',function () {
+    $(".img_back_"+className).css({"backgroundImage":"url(./img/frame_2_0.png)","transform": "rotate(0)"})
+    $(".img_show_"+className).find('img').attr('src','img/'+className+'_2_1.png')
+    $(".key_value_"+className).find('p').css({'color':'#398fd1'})
+    $(".val_"+className).css({'color':'#777777'})
+    $(".key_"+className).css({'color':'#222222'})
     $(".img_back_"+className).rotate({
-      animateTo: 90,
-      callback: function () {
-        $(this).css({"backgroundImage":"url(./img/frame_2_0.png)","transform": "rotate(0)"})
-        $(".img_show_"+className).find('img').attr('src','img/'+className+'_2_1.png')
-        $(".key_value_"+className).find('p').css({'color':'#398fd1'})
-        $(".val_"+className).css({'color':'#777777'})
-        $(".key_"+className).css({'color':'#222222'})
-      }
+      animateTo: 90
     })
   })
 }
